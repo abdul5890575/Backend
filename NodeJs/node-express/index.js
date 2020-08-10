0470b9e7400f8e552c11556e9f8f47166870d286
Abdul Rehman Shaukat <abdulrehman.shoukat@gmail.com>
2020-08-09 22:45:56 -0400

    before assi

---
+++ NodeJs/node-express/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http= require('http');
+const morgan = require('morgan');
+const bodyParser= require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const hostname='localhost';
+const port = 3000;
+
+const app= express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes',dishRouter);
+
+app.get('/dishes/:dishId',(req,res,next)=>{
+    res.end('Will send detail of the dish:' + req.param.dishId + 'to you');
+});
+
+app.post('/dishes/:dishId',(req,res,next)=>{
+    res.statusCode= 403;
+    res.end('Post not supported' + req.param.dishId);
+});
+
+app.put('/dishes/:dishId',(req,res,next)=>{
+    res.write(' Updating :' + req.param.dishId + '\n');
+    res.end('Will update' + req.body.name + 'with details' + req.body.description);
+});
+
+app.delete('/dishes/:dishId',(req,res,next)=>{
+    res.end('Deleting dishe' + req.param.dishId);
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next) => {
+    res.statusCode=200;
+    res.setHeader('Content-type','text/html');
+    res.end('<html><body><h1>This is express server</h1></body></html>');
+});
+
+const server= http.createServer(app);
+
+server.listen(port,hostname,()=>{
+    console.log(`Server running http://${hostname}:${port}`)
+})
