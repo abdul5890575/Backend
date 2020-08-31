30d890e2be8500f006bb9e6668076c52db3091df
Abdul Rehman Shaukat <abdulrehman.shoukat@gmail.com>
2020-08-30 21:06:54 -0400

    cross and uploading

--- NodeJs/conFusionServer/app.js
+++ NodeJs/conFusionServer/app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leadersRouter = require('./routes/leadersRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const Dishes=require('./models/dishes');
@@ -30,6 +31,14 @@ connect.then((db)=> {
 
 var app = express();
 
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    return next();
+  }
+  else{
+    res.redirect(307, 'https://'+ req.hostname+ ':' + app.get('secPort')+ req.url);
+  }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -51,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leadersRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
